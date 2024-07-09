0222dd50267fc75fed8bfaf94ef39104c0a6c98b
Algis <balbatunas1@gmail.com>
2024-07-09 15:01:00 +0300

    Uploading recipe Spring Booot project

---
+++ recipe/src/test/java/lt/techin/recipe/RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.techin.recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
