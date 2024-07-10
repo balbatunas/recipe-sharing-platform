c809ca9c96b4da80e0cecbd5304a150120e183d0
Julius Zabulėnas <jzabulenas@protonmail.com>
2024-07-10 16:52:00 +0300

    Add routes for homepage, register components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Register from "./components/Register";
+import Homepage from "./components/Homepage";
 
 function App() {
   return (
@@ -7,6 +10,20 @@ function App() {
       <header>
         <Navbar />
       </header>
+
+      <main className="container">
+        <Routes>
+          <Route
+            element={<Homepage />}
+            path="/"
+          />
+
+          <Route
+            element={<Register />}
+            path="/register"
+          />
+        </Routes>
+      </main>
     </>
   );
 }
