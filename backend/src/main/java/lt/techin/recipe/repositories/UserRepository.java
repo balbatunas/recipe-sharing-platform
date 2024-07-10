1b3b44cd76077730c7327a875631fb35def208ad
Algis <balbatunas1@gmail.com>
2024-07-10 15:51:20 +0300

    Corrected UserRepository that only existByEmail left

--- backend/src/main/java/lt/techin/recipe/repositories/UserRepository.java
+++ backend/src/main/java/lt/techin/recipe/repositories/UserRepository.java
@@ -3,14 +3,7 @@ package lt.techin.recipe.repositories;
 import lt.techin.recipe.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface UserRepository extends JpaRepository<User, Integer> {
-    Optional<User> findUserByEmail(String email);
-
-    Optional<User> findUserByEmailAndPassword(String email, String password);
-
-    //    boolean findUserByUsernameAndPassword(String name, String password);
 
     boolean existsByEmail(String name);
 }
