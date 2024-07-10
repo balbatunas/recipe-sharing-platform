5db70a5942d39fb05b4e7b64987d2e5d9c1464c1
Julius Zabulėnas <jzabulenas@protonmail.com>
2024-07-10 15:45:05 +0300

    Add aria attributes for accessibility, active class

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,6 +14,9 @@ export default function Navbar() {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -25,8 +28,9 @@ export default function Navbar() {
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <a
-                className="nav-link"
+                className="nav-link active"
                 href="#"
+                aria-current="page"
               >
                 Home
               </a>
