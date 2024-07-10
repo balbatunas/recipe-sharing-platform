7a84fe3a525124cb630cf252521aa08a6af125d6
Algis <balbatunas1@gmail.com>
2024-07-10 15:47:32 +0300

    Corrected that only required Post mapping left in User controller

--- backend/src/main/java/lt/techin/recipe/controllers/UserController.java
+++ backend/src/main/java/lt/techin/recipe/controllers/UserController.java
@@ -6,11 +6,12 @@ import lt.techin.recipe.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
 
 @CrossOrigin
 @RestController
@@ -23,23 +24,6 @@ public class UserController {
     public void UserRepository(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-    }
-
-    @GetMapping("/users")
-    public ResponseEntity<List<User>> getUsers() {
-        return ResponseEntity.status(200).body(this.userRepository.findAll());
-    }
-
-    @GetMapping("/users/{id}")
-    public ResponseEntity<?> getUser(@PathVariable int id) {
-        if (userRepository.existsById(id)) {
-            return ResponseEntity.status(200)
-                    .body(this.userRepository.findById(id).get());
-        }
-        HashMap<String, String> response = new HashMap<>();
-        response.put("message", "User with Id " + id + " does not exist");
-
-        return ResponseEntity.status(404).body(response);
     }
 
     @PostMapping("/users")
@@ -56,40 +40,5 @@ public class UserController {
         return ResponseEntity.status(201).body(this.userRepository.save(user));
     }
 
-    @PostMapping("/login")
-    public Optional<User> login(@RequestBody User user) {
 
-        Optional<User> userDb = this.userRepository.findUserByEmail(user.getUsername());
-        if (passwordEncoder.matches(user.getPassword(), userDb.get().getPassword())) {
-            return userDb;
-        }
-        return null;
-    }
-
-    @PutMapping("/users/{id}")
-    public ResponseEntity<?> putUser(@Valid @PathVariable int id, @RequestBody User user) {
-        if (this.userRepository.findById(id).isPresent()) {
-            User userFromList = this.userRepository.findById(id).get();
-
-            userFromList.setEmail(user.getEmail());
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            userFromList.setPassword(user.getPassword());
-
-            return ResponseEntity.status(200).body(this.userRepository.save(userFromList));
-        }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return ResponseEntity.status(201).body(this.userRepository.save(user));
-    }
-
-    @DeleteMapping("/users/{id}")
-    public ResponseEntity<?> deleteUser(@PathVariable int id) {
-        if (userRepository.existsById(id)) {
-            this.userRepository.deleteById(id);
-            return ResponseEntity.status(204).build();
-        }
-        HashMap<String, String> response = new HashMap<>();
-        response.put("message", "User with Id " + id + " does not exist");
-
-        return ResponseEntity.status(404).body(response);
-    }
 }
