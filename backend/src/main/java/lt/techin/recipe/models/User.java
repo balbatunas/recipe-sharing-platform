e1188ea1a8c29b2b5ea61e75fc89ada82dc70980
Julius <jzabulenas@protonmail.com>
2024-07-13 17:15:38 +0300

    Add NotEmpty annotation to roles

--- backend/src/main/java/lt/techin/recipe/models/User.java
+++ backend/src/main/java/lt/techin/recipe/models/User.java
@@ -70,7 +70,7 @@ public class User implements UserDetails {
             name = "Users_roles",
             joinColumns = @JoinColumn(name = "User_id"),
             inverseJoinColumns = @JoinColumn(name = "Role_id"))
-    //    @NotNull
+    @NotEmpty(message = "Cannot be null or empty")
     private List<Role> roles;
 
     public User(
