ee3b2671247352bb1cfa223cd317d897010717ee
Algis <balbatunas1@gmail.com>
2024-07-19 11:11:35 +0300

    Add succsess message aftrer registration

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,3 +1,15 @@
+import { useLocation } from "react-router-dom";
+
 export default function Homepage() {
-  return <p>welcome to home</p>;
+  const location = useLocation();
+  const message = location.state?.message;
+
+  return (
+    <div>
+      <h4>Welcome to HomePage</h4>
+      {/* Other components and content */}
+      <br />
+      {message && <h2>{message}</h2>}
+    </div>
+  );
 }
