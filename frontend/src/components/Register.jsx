dde247466f19a9b20c859145312b842c9f67fcb4
Algis <balbatunas1@gmail.com>
2024-07-26 16:21:01 +0300

    Fix mistake in word Mmaximum

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -220,7 +220,7 @@ export default function Register() {
             <div className="text-danger">This field is required</div>
           )}
           {errors.email?.type === "maxLength" && (
-            <div className="text-danger">Mmaximum length 200 characters</div>
+            <div className="text-danger">Maximum length 200 characters</div>
           )}
           {errors.email?.type === "minLength" && (
             <div className="text-danger">Minimum length 5 characters</div>
